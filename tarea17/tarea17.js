7ed61e378c9cc233c9bcdbd03390fa76f45c5aa8
jairpugliese <112570064+jairpugliese@users.noreply.github.com>
2024-10-06 18:43:45 -0500

    syle

---
+++ tarea17/tarea17.js
@@ -0,0 +1,15 @@
+function mostrarMensaje(){
+    alert("HOLA. Bienvenidos");
+}
+
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event) {
+ const name = document.getElementById('name').value;
+ const email = document.getElementById('email').value;
+ if (name === '' || email === '') {
+ alert('Por favor, completa todos los campos.');
+ event.preventDefault();
+ } else {
+ alert('¡Formulario enviado correctamente!');
+ }
+});
